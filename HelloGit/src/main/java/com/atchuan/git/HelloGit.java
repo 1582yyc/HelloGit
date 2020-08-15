cdd8e73326567297d520a0025b557190554ee4b5
adfg <1446469365@qq.com>
2020-08-15 17:57:40 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atchuan/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.atchuan.git;
+
+/**
+ * @author shkstart
+ * @create 2020-08-15 17:49
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+
+    }
+
+
+}
